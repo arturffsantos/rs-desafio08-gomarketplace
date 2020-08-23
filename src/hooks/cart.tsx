b2903d477d292d6e5cf0eb59a62a562fbfc3a2d6
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-08-23 18:29:47 -0300

    load products from storage

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,7 +30,13 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storedProducts = await AsyncStorage.getItem(
+        '@GoMarketplace:products',
+      );
+
+      if (storedProducts) {
+        setProducts([...JSON.parse(storedProducts)]);
+      }
     }
 
     loadProducts();
