2c3d0da7c374e37a756e2d09f8cd96ae9265b7e8
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2020-08-23 19:25:44 -0300

    calculate cart total value in Cart view

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,9 +47,11 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((sum, { price, quantity }) => {
+      return sum + price * quantity;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
